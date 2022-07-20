e939e1d0cce98f0e582e98d061d8b3a30b780c5a
Matt Lehrer <lehrerm@gmail.com>
2022-07-20 10:37:23 +0200

    fixes

--- preset.ts
+++ preset.ts
@@ -4,16 +4,32 @@ export default definePreset({
 		// ...
 	},
 	handler: async () => {
+		await executeCommand({
+			command: 'git',
+			arguments: ['init'],
+		});
+		await executeCommand({
+			command: 'git',
+			arguments: ['add', '.'],
+		});
+		await executeCommand({
+			command: 'git',
+			arguments: ['commit', '-a', '-m', '"From svelte-add"'],
+		});
 		await extractTemplates();
+		await executeCommand({
+			command: 'pnpm',
+			arguments: ['rm', 'prettier-plugin-svelte'],
+		});
 		await installPackages({
 			for: 'node',
 			packages: ['prettier-plugin-tailwindcss'],
 			dev: true,
 		});
+		await deletePaths({ paths: ['.prettierrc'] });
 		await executeCommand({
 			command: 'pnpm',
-			arguments: ['rm prettier-plugin-svelte'],
+			arguments: ['i'],
 		});
-		await deletePaths({ paths: ['.prettierrc'] });
 	},
 });
