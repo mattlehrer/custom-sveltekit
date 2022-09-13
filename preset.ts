2d3dfeaf63cd3fa3b780a8b732ed89f48bccabf2
Matt Lehrer <lehrerm@gmail.com>
2022-09-13 15:21:00 +0200

    setup eslint-plugin-tailwindcss and name steps

--- preset.ts
+++ preset.ts
@@ -2,49 +2,88 @@ export default definePreset({
 	name: 'custom-sveltekit',
 	options: {},
 	handler: async () => {
-		await executeCommand({
-			command: 'git',
-			arguments: ['init'],
+		await group({
+			title: 'initialize repository',
+			handler: async () => {
+				await executeCommand({
+					command: 'git',
+					arguments: ['init'],
+				});
+				await executeCommand({
+					command: 'git',
+					arguments: ['add', '.'],
+				});
+				await executeCommand({
+					command: 'git',
+					arguments: ['commit', '-a', '-m', '"From svelte-add"'],
+				});
+			}
 		});
-		await executeCommand({
-			command: 'git',
-			arguments: ['add', '.'],
+		await group({
+			title: 'extract template',
+			handler: async () => {
+				await extractTemplates();
+			}
 		});
-		await executeCommand({
-			command: 'git',
-			arguments: ['commit', '-a', '-m', '"From svelte-add"'],
+		await group({
+			title: 'setup prettier and eslint plugins',
+			handler: async () => {
+				await executeCommand({
+					command: 'pnpm',
+					arguments: ['rm', 'prettier-plugin-svelte'],
+				});
+				await installPackages({
+					for: 'node',
+					packages: ['prettier-plugin-tailwindcss', 'eslint-plugin-tailwindcss'],
+					dev: true,
+				});
+				await editFiles({
+					files: ['.eslintrc.cjs'], operations: [{
+						type: 'update-content',
+						update: (content) => {
+							return content.replace(/extends: \[(.*?)('|',\n\s)\],/gsm, "extends: [$1', 'plugin:tailwindcss/recommended'],").replace(/plugins: \[(.*)\],/g, "plugins: [$1, 'tailwindcss'],");
+						}
+					}]
+			
+				})
+				await deletePaths({ paths: ['.prettierrc'] });
+			}
 		});
-		await extractTemplates();
-		await executeCommand({
-			command: 'pnpm',
-			arguments: ['rm', 'prettier-plugin-svelte'],
+		await group({
+			title: 'install packages',
+			handler: async () => {
+				await executeCommand({
+					command: 'pnpm',
+					arguments: ['i'],
+				});
+			}
 		});
-		await installPackages({
-			for: 'node',
-			packages: ['prettier-plugin-tailwindcss'],
-			dev: true,
+		await group({
+      title: 'format files',
+			handler: async () => {
+				await executeCommand({
+					command: 'pnpm',
+					arguments: ['format'],
+				});
+			}
 		});
-		await deletePaths({ paths: ['.prettierrc'] });
-		await executeCommand({
-			command: 'pnpm',
-			arguments: ['i'],
-		});
-		await executeCommand({
-			command: 'pnpm',
-			arguments: ['format'],
-		});
-		await executeCommand({
-			command: 'git',
-			arguments: ['add', '.'],
-		});
-		await executeCommand({
-			command: 'git',
-			arguments: [
-				'commit',
-				'-a',
-				'-m',
-				'"From preset: mattlehrer/custom-sveltekit"',
-			],
+		await group({
+			title: 'commit changes',
+			handler: async () => {
+				await executeCommand({
+					command: 'git',
+					arguments: ['add', '.'],
+				});
+				await executeCommand({
+					command: 'git',
+					arguments: [
+						'commit',
+						'-a',
+						'-m',
+						'"From preset: mattlehrer/custom-sveltekit"',
+					],
+				});
+			}
 		});
 	},
 });
